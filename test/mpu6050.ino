7495fbb93a0f9733268c759df2201056e4beffe6
Maura Yufi Septania Putri <maurayufi@mail.ugm.ac.id>
2023-09-19 16:01:50 +0700

    add test file

---
+++ test/mpu6050.ino
@@ -0,0 +1,30 @@
+#include <MPU6050_tockn.h>
+#include <Wire.h>
+
+MPU6050 mpu6050(Wire);
+
+long timer = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets(true);
+}
+
+void loop() {
+  mpu6050.update();
+
+  if(millis() - timer > 10){
+    Serial.print("angleX : ");Serial.print(mpu6050.getAngleX());
+    Serial.print("\tangleY : ");Serial.print(mpu6050.getAngleY());
+    Serial.print("\tangleZ : ");Serial.println(mpu6050.getAngleZ());
+    Serial.print("\accX : ");Serial.print(mpu6050.getAccX());
+    Serial.print("\taccY : ");Serial.print(mpu6050.getAccY());
+    Serial.print("\taccZ : ");Serial.println(mpu6050.getAccZ());
+
+    Serial.println("");
+    delay(1000);
+    timer = millis();
+  }
+}
