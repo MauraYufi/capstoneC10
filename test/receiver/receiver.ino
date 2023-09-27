249e766f7ce754363b14e95c644c8f2b1ceef808
Maura Yufi Septania Putri <maurayufi@mail.ugm.ac.id>
2023-09-27 15:15:03 +0700

    ver 1

--- test/receiver/receiver.ino
+++ test/receiver/receiver.ino
@@ -11,8 +11,9 @@ const byte thisSlaveAddress[5] = {'R','x','A','A','A'};
 
 RF24 radio(CE_PIN, CSN_PIN);
 
-char dataReceived[10]; // this must match dataToSend in the TX
+float dataReceived; // this must match dataToSend in the TX
 bool newData = false;
+int dataToSend=1;
 
 //===========
 
@@ -23,19 +24,36 @@ void setup() {
     Serial.println("SimpleRx Starting");
     radio.begin();
     radio.setDataRate( RF24_250KBPS );
-    radio.openReadingPipe(1, thisSlaveAddress);
-    radio.startListening();
+    //radio.openReadingPipe(1, thisSlaveAddress);
+    radio.openWritingPipe(thisSlaveAddress);
+    //radio.startListening();
 }
 
 //=============
 
 void loop() {
+    bool rslt;
+    rslt = radio.write( &dataToSend, sizeof(dataToSend) );
+        // Always use sizeof() as it gives the size as the number of bytes.
+        // For example if dataToSend was an int sizeof() would correctly return 2
 
-    getData();
-    showData();
+    Serial.print("Data Sent ");
+    Serial.print(dataToSend);
+    if (rslt) {
+        Serial.println("  Acknowledge received");
+        radio.openReadingPipe(1, thisSlaveAddress);
+        radio.startListening();
+        getData();
+        showData();
+    }
+    else {
+        Serial.println("  Tx failed");
+    }
+
 }
 
 //==============
+
 
 void getData() {
     if ( radio.available() ) {
