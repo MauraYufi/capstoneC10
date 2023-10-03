79f7b75c35436b133795b23beb95270e4d13402c
cokagung <60917457+cokagung@users.noreply.github.com>
2023-10-03 17:44:18 +0700

    Update esp.ino

--- test/esp/esp.ino
+++ test/esp/esp.ino
@@ -4,7 +4,7 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-
+#include <math.h>
 
 #define CE_PIN   26
 #define CSN_PIN 27
@@ -17,6 +17,18 @@ RF24 radio(CE_PIN, CSN_PIN); // Create a Radio
 float PembacaanTG=0;
 int dataTrigger=1;
 char txNum = '0';
+float wx0;
+float wy0;
+float wz0;
+float wx1;
+float wy1;
+float wz1;
+float xn;
+float ax;
+float ay;
+float az;
+float a;
+float h;
 
 
 unsigned long currentMillis;
@@ -76,13 +88,37 @@ void loop() {
 
 void send() {
     bool rslt;
-    PembacaanTG = mpu6050.getAngleX();
+    wx0 = mpu6050.getAngleX();
+    wy0 = mpu6050.getAngleY();
+    wz0 = mpu6050.getAngleZ();
+    Serial.println(wx0);
+    Serial.println(wy0);
+    Serial.println(wz0);
+    
+    delay(1000);
+    mpu6050.update();
+    wx1 = mpu6050.getAngleX();
+    wy1 = mpu6050.getAngleY();
+    wz1 = mpu6050.getAngleZ();
+    Serial.println(wx1);
+    Serial.println(wy1);
+    Serial.println(wz1);
+    ax = mpu6050.getAccX();
+    ay = mpu6050.getAccY();
+    az = mpu6050.getAccZ();
+    a = sqrt((pow(ax/9.806,2)+pow(ay/9.806,2)+pow(az/9.806,2)));
+    xn = sqrt((pow((wx1-wx0)*0.0175,2)+pow((wy1-wy0)*0.0175,2)+pow((wz1-wz0)*0.0175,2)));
+    h = a/pow(xn,2);
+    PembacaanTG = h;
     rslt = radio.write( &PembacaanTG, sizeof(PembacaanTG) );
         // Always use sizeof() as it gives the size as the number of bytes.
         // For example if dataToSend was an int sizeof() would correctly return 2
 
-    Serial.print("Data Sent ");
-    Serial.print(PembacaanTG);
+    Serial.println("Data Sent ");
+    Serial.println(PembacaanTG);
+    Serial.println(a);
+    Serial.println(xn);
+    // Serial.println(h);
     if (rslt) {
         Serial.println("  Acknowledge received");
     }
